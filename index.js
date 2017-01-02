cc5f601ca68e9efe109c3281d0267669944ffeb1
Ewerton <3w3rt0n@gmail.com>
2017-01-02 16:55:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ var server = http.createServer(function(request, response){
 	response.writeHead(200, {"Content-Type": "text/html"});
 	if(request.url == "/"){
 		response.write("<h1>Página principal</h1>");
+		console.log('Pagina inicial');
 	}else if(request.url == "/bemvindo"){
 		response.write("<h1>Bem-vindo :)</h1>");
+		console.log('Pagina bem vindo');
 	}else{
 		response.write("<h1>Página não encontrada :(</h1>");
+		console.log('Pagina nao encontrada');
 	}
 	response.end();
 });
